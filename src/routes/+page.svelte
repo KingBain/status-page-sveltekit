134fe74ff36b327210ff8685b81262e7b9c5e778
John Bain <john.bain@gmail.com>
2025-03-18 19:17:15 +0000

    basic index page running

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import snarkdown from "snarkdown";
+  import ActiveIncidents from "$lib/components/ActiveIncidents.svelte";
+  import ActiveScheduled from "$lib/components/ActiveScheduled.svelte";
+  import Incidents from "$lib/components/Incidents.svelte";
+  import LiveStatus from "$lib/components/LiveStatus.svelte";
+  import Scheduled from "$lib/components/Scheduled.svelte";
+  import config from "$lib/data/config.json";
+
+  let title = "Status";
+  try {
+    title = config["status-website"].name;
+  } catch (error) {}
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<header>
+  {#if config["status-website"]}
+    {#if config["status-website"].introTitle}
+      <h1>
+        {@html snarkdown(config["status-website"].introTitle)}
+      </h1>
+    {/if}
+    {#if config["status-website"].introMessage}
+      <p class="lead">
+        {@html snarkdown(config["status-website"].introMessage)}
+      </p>
+    {/if}
+  {/if}
+</header>
+
+<ActiveIncidents />
+<ActiveScheduled />
+<LiveStatus />
+<Scheduled />
+<Incidents />
+
+<style>
+  p.lead {
+    font-size: 110%;
+  }
+  header {
+    margin-bottom: 2rem;
+  }
+</style>
---
+++ src/routes/incident/[number]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  // 'data' is automatically passed in from the load function
+  export let data;
+  import Incident from "$lib/components/Incident.svelte";
+  
+  // Extract 'number' from the 'data'
+  const { number } = data;
+</script>
+
+<Incident {number} />
