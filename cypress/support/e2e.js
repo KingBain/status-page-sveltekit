680173ba03f247b36fd9fe3c71ab161406105549
John Bain <john.bain@gmail.com>
2025-03-18 22:11:25 +0000

    updated

---
+++ cypress/support/e2e.js
@@ -0,0 +1,6 @@
+// cypress/support/e2e.js
+
+// Import commands if you have them, for example:
+import './commands';
+
+// You can also set up global behavior here if needed.
---
+++ init-tests.ts
@@ -0,0 +1,12 @@
+import fsExtra from "fs-extra";
+import { join } from "path";
+
+const initTests = async () => {
+  let hasFile = false;
+  try {
+    await fsExtra.readFile(join("..", ".upptimerc.yml"));
+    hasFile = true;
+  } catch (error) {}
+  if (!hasFile) await fsExtra.copyFile(join(".", ".upptimerc.yml"), join("..", ".upptimerc.yml"));
+};
+initTests();
