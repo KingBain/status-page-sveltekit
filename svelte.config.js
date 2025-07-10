235577800f72073404a45e69537b250b401a9fb6
John Bain <john.bain@gmail.com>
2025-07-10 02:15:08 +0000

    updated path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
+import siteConfig from './src/lib/data/config.json' with { type: 'json' };
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +11,9 @@ const config = {
 		alias: {
 			$lib: path.resolve('./src/lib')
 		},
+		paths: {
+			base: siteConfig.baseUrl || ''
+		  },
 		adapter: adapter({
 			pages: '__sapper__/export',
 			assets: '__sapper__/export',
