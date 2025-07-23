8118f034c33773ef74eca624764979d7bb6dfc08
John Bain <John.Bain@phac-aspc.gc.ca>
2025-07-23 01:10:12 +0000

    fixed config errors

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,17 @@
 // src/global.d.ts
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.svelte' {
   import { SvelteComponentTyped } from 'svelte';
   export default class Component extends SvelteComponentTyped<
-    // no props by default — override with your own types if needed
     Record<string, any>,
     Record<string, any>,
     Record<string, any>
   > {}
+}
+
+
+declare module '$lib/data/config.json' {
+  const value: any;
+  export default value;
 }
